b2267e3638fda844c24305616655d96e59aba99b
paosder <denjaraos@gmail.com>
2021-04-18 18:30:22 +0900

    fix compatibility issue (cjs)

---
+++ esbuild.config.mjs
@@ -0,0 +1,30 @@
+import esbuild from 'esbuild';
+
+esbuild.build({
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  format: 'esm',
+  sourcemap: true,
+  outfile: 'dist/esm/index.js',
+  target: 'es2020',
+  plugins: [],
+  loader: {},
+  external: [],
+}).catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
+
+esbuild.build({
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  format: 'cjs',
+  sourcemap: true,
+  outfile: 'dist/cjs/index.js',
+  plugins: [],
+  loader: {},
+  external: [],
+}).catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
